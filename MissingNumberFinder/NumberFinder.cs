a35f1009578d6467d3cc770690e5786c94012832
Chris Mullins <cmullins@gmail.com>
2019-04-30 23:16:14 -0700

    Cleaned up code & tests

--- MissingNumberFinder/NumberFinder.cs
+++ MissingNumberFinder/NumberFinder.cs
@@ -4,7 +4,7 @@ namespace MissingNumberFinder
 {
     public static class NumberFinder
     {
-        private static void AssertValidSequence(int [] sequence)
+        private static void AssertValidSequence(int[] sequence)
         {
             if (sequence == null)
             {
@@ -19,16 +19,19 @@ namespace MissingNumberFinder
             }
         }
 
-        public static int FindDelta(int [] sequence)
+        /// <summary>
+        /// Given a list of numbers all spaced out by the same value, one of those values is missing. This
+        /// function determines the delta between numbers via formula. 
+        /// For a sequence of 0, 1, or 2 numbers this problem isn't valid. 
+        /// Examples: 
+        /// { 1,2,3,4,6 } (Note: The 5 is missing).  Δ = 1
+        /// { 2,4,8 } (Note: 6 is missing). Δ = 2
+        /// </summary>
+        /// <param name="sequence">The array of whole numbers to analyze.</param>
+        /// <returns>The whole nubmer representing the delta (Δ) value.</returns>
+        public static int FindDelta(int[] sequence)
         {
-            // Given a list of numbers all spaced out by the same value, one of those values is missing. This
-            // function determines the delta between numbers. 
-            // For a sequnce of 0, 1, or 2 numbers this problem isn't valid. 
-            // Examples: 
-            // { 1,2,3,4,6 } (Note: The 5 is missing).  Δ = 1
-            // { 2,4,8 } (Note: 6 is missing). Δ = 2
-
-            // Make sure the passed in params are valid
+            // Make sure the passed in params are somewhat sensical.
             AssertValidSequence(sequence);
 
             var startFinishDelta = sequence[sequence.Length - 1] - sequence[0];
@@ -37,7 +40,18 @@ namespace MissingNumberFinder
             return delta;
         }
 
-        public static int FindMissingValue( int [] sequence)
+        /// <summary>
+        /// Finds the missing value in a sequence of whole numbers. 
+        /// </summary>
+        /// <remarks>
+        /// Uses a basic binary search to scan the array lookig for the missing 
+        /// value. The key observation is that we know what the value at a given
+        /// index **should** be, and so we can quickly find the one that's 
+        /// incorrect.                
+        /// </remarks>
+        /// <param name="sequence">The sequence of whole numbers (with one missing) to analyze.</param>
+        /// <returns>The missing value.</returns>
+        public static int FindMissingValue(int[] sequence)
         {
             // Make sure the passed in params are valid
             AssertValidSequence(sequence);
@@ -46,34 +60,33 @@ namespace MissingNumberFinder
             int delta = FindDelta(sequence);
             if (delta == 0)
             {
-                throw new InvalidOperationException("Not allowed for a zero delta.");
+                throw new InvalidOperationException("A delta of Zero is not valid.");
             }
 
             int startIndex = 0;
             int endIndex = sequence.Length - 1;
             while (startIndex <= endIndex)
             {
-                // 0, 1, 3
-                // Expected value at:
-                // [0] = 0
-                // [1] = 0 + ([1] * 1)
-                // [2] = 0 + (2 * 1)
-
-                // {1, 3, 4 }                
-
                 int midpoint = (startIndex + endIndex) / 2;
-                int expectedValue = sequence[0] + (midpoint * delta);
-                if (sequence[midpoint] == ExpectedValueAtIndex(sequence, midpoint, delta))
+                int expectedValue = ExpectedValueAtIndex(sequence, midpoint, delta);
+
+                if (sequence[midpoint] == expectedValue)
                 {
-                    // Everything up to this point is good. Look to the right. 
+                    // Everything up to this point is good. Look to the right in the set. 
                     startIndex = midpoint + 1;
                 }
-                else if (sequence[midpoint-1] == ExpectedValueAtIndex(sequence, midpoint-1, delta))
+                else if (sequence[midpoint - 1] == ExpectedValueAtIndex(sequence, midpoint - 1, delta))
                 {
+                    // Expected value is not correct at the current index, 
+                    // yet the value just to the left of us is correct. That means we are 
+                    // on the first incorrect index. Return the expected value. 
                     return expectedValue;
                 }
                 else
                 {
+                    // Expected value is not correct at the current index, 
+                    // Yet the value just to the left of us is *ALSO* incorrect, 
+                    // meaning the problem lies somewhere in the left half of the set.  
                     endIndex = midpoint - 1;
                 }
             }
@@ -81,7 +94,7 @@ namespace MissingNumberFinder
             throw new Exception("No Missing value found");
         }
 
-        private static int ExpectedValueAtIndex(int [] sequence, int index, int delta)
+        private static int ExpectedValueAtIndex(int[] sequence, int index, int delta)
         {
             return sequence[0] + (index * delta);
         }
--- MissingNumberFinderTests/FindMissingNumberTests.cs
+++ MissingNumberFinderTests/FindMissingNumberTests.cs
@@ -19,16 +19,6 @@ namespace MissingNumberFinderTests
         }
 
         [Theory] // [item, item, item, MISSING NUMBER]
-        [InlineData(1, 2, 3)]
-        [InlineData(1, 2, 1)]
-        [InlineData(2, 4, 9)]
-        public void Throws_On_Invalid_Lists(int value0, int value1, int value2)
-        {
-            int[] values = new int[] { value0, value1, value2 };           
-            Assert.Throws<InvalidOperationException>( () => NumberFinder.FindMissingValue(values));
-        }
-
-        [Theory] // [item, item, item, MISSING NUMBER]
         [InlineData(1, 2, 4, 3)]
         [InlineData(1, 3, 4, 2)]
         [InlineData(10, 12, 13, 11)]
@@ -53,7 +43,7 @@ namespace MissingNumberFinderTests
         [Theory] // [item, [...], item, MISSING NUMBER]
         [InlineData(1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 10)] // 10 missing (right end)
         [InlineData(1, 3, 4, 5, 6, 7, 8, 9, 10, 11, 2)] // 2 missing (left end)
-        [InlineData(1, 2, 3, 4, 6, 7, 8, 9, 10, 11, 5)] // 10 missing (midpoint)
+        [InlineData(1, 2, 3, 4, 6, 7, 8, 9, 10, 11, 5)] // 5 missing (midpoint)
         public void Correct_Results_For_10_Item_Lists(int value0, int value1, int value2, int value3,
                                                         int value4, int value5, int value6,
                                                         int value7, int value8, int value9,
